dc8a234e283956c1abb21d540318982d1debc490
GayBoiJarleru <54669305+GayBoiJarleru@users.noreply.github.com>
2020-05-15 14:33:54 +0200

    Add files via upload

--- src/GissaTalet.java
+++ src/GissaTalet.java
@@ -1,8 +1,45 @@
 import javax.swing.*;
-
+import java.util.Random;
 public class GissaTalet {
-    public void main(String[] args) {
-        
+    public static void main(String[] args) {
+
+
+        Random random = new Random();
+        int Random = random.nextInt(100);
+        while (true) {
+            String Pang = JOptionPane.showInputDialog(null, "skriv ett tal mellan 1 och 100");
+            int Tal = Integer.parseInt(Pang);
+            if (Random == Tal) {
+                JOptionPane.showMessageDialog(null, "rätt");
+                break;
+            } else if (Random > Tal) {
+                JOptionPane.showMessageDialog(null, "Gissa högre");
+            } else {
+                JOptionPane.showMessageDialog(null, "Gissa Lägre");
+            }
+
+        }
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
